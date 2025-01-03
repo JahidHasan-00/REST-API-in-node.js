48a5b56ad15d687b451b417d06248740876212a8
JahidHasan-00 <jahid.hasan50001@gmail.com>
2025-01-03 18:43:34 +0600

    Post request is created

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const app =  express();
+app.use(express.json());
+
+const articles = [
+    {
+        'id': 100,
+        'name': 'John Doe',
+        'email': 'jahid.hasan50001@gmail.com'
+    },
+    {
+        'id': 101,
+        'name': 'Jahid Hasan',
+        'email': 'jahid.hasan50001@gmail.com'
+    }
+]
+
+// article routes --> localhost:6000/articles
+
+app.get('/articles', (req, res) => {
+    res.json({
+        data: articles
+    })
+})
+
+app.post('/articles', (req, res) => {
+    console.log(req.body);
+})
 
 app.get('/', (req, res) => {
     res.json({message: 'Hello World! This is a simple Node.js application.'});
